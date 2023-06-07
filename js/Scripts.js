96bb49daf52053ff79b735c005bef18459e30d9e
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-07 16:10:17 +0200

    {feat- add pokemon data}

---
+++ js/Scripts.js
@@ -0,0 +1,15 @@
+
+//array of objects
+var pokemonList = [{
+    name: "Caterpie", height: 3, types: ["Bug", "Runway"], weight: 1
+}, {
+    name: "Ivysaur", height: 1, types: ["Grass", "poision"], weight: 13
+}, {
+    name: "Butterfree", height: 2, types: ["Bug", "Flying"], weight: 32
+}, {
+    name: "Gloom", height: 1, types: ["Grass", "Poision"], weight: 9
+}, {
+    name: "Persian", height: 1, types: ["Limber", "Unnerve"], weight: 32
+}]
+
+console.log(pokemonList[0].name);
