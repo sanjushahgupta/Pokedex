992319a9d55f68eadaa17da37667c0e20d969d00
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-08 22:54:03 +0200

    refactor pokemonList

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,27 +1,44 @@
 
-//array of objects
-var pokemonList = [{
-    name: "Caterpie", height: 3, types: ["Bug", "Runway"], weight: 1
-}, {
-    name: "Ivysaur", height: 1, types: ["Grass", "poision"], weight: 13
-},
-{
-    name: "Persian", height: 12, types: ["Limber", "Unnerve"], weight: 32
-},
-{
-    name: "Butterfree", height: 7, types: ["Bug", "Flying"], weight: 32
-}, {
-    name: "Gloom", height: 6, types: ["Grass", "Poision"], weight: 9
-}]
+//exercise 1.5
+var pokemonRepository = (function () {
+    let pokemonList = [{
+        name: "Caterpie", height: 3, types: ["Bug", "Runway"], weight: 1
+    }, {
+        name: "Ivysaur", height: 1, types: ["Grass", "poision"], weight: 13
+    },
+    {
+        name: "Persian", height: 12, types: ["Limber", "Unnerve"], weight: 32
+    },
+    {
+        name: "Butterfree", height: 7, types: ["Bug", "Flying"], weight: 32
+    }, {
+        name: "Gloom", height: 6, types: ["Grass", "Poision"], weight: 9
+    }]
 
-
-for (var i = 0; i < pokemonList.length; i++) {
-
-    //If height is more than 9 then only print wow that's a big.
-    if (pokemonList[i].height > 9) {
-        document.write(pokemonList[i].name + " has height of " + pokemonList[i].height + "<b> -Wow that's big!</b><br><br>")
-    } else {
-        document.write(`${pokemonList[i].name} has a height of ${pokemonList[i].height}.<br><br>`);
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    }
 }
+)();
+
+pokemonRepository.getAll().forEach(item => {
+    let result = item.height > 9 ? "<b> -Wow that's big!</b>"
+        : ""
+
+    document.write(`${item.name} has height of ${item.height} ${result} <br>`);
+});
+
+
+
+
+
+
+
+
