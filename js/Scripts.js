679926374e960598a7421847fa8610f9deb70045
Sanju Shah <sanjushahgupta@gmail.com>
2023-06-09 11:25:46 +0200

    refactor pokemonList

--- js/Scripts.js
+++ js/Scripts.js
@@ -1,5 +1,4 @@
 
-//exercise 1.5
 var pokemonRepository = (function () {
     let pokemonList = [{
         name: "Caterpie", height: 3, types: ["Bug", "Runway"], weight: 1
@@ -15,25 +14,41 @@ var pokemonRepository = (function () {
         name: "Gloom", height: 6, types: ["Grass", "Poision"], weight: 9
     }]
 
+
     function add(pokemon) {
-        pokemonList.push(pokemon);
+
+        if (typeof pokemon === typeof pokemonList) {
+            pokemonList.push(pokemon);
+            // console.log(Object.keys(pokemon));
+            // console.log(Object.keys(pokemonList[0]));
+        }
     }
+
     function getAll() {
         return pokemonList;
     }
+
     return {
         add: add,
         getAll: getAll
     }
+
 }
 )();
+
+pokemonRepository.add({ name: "New Bee", height: 1, types: ["Grass", "Poision"], weight: 9 });
 
 pokemonRepository.getAll().forEach(item => {
     let result = item.height > 9 ? "<b> -Wow that's big!</b>"
         : ""
-
     document.write(`${item.name} has height of ${item.height} ${result} <br>`);
 });
+
+
+
+
+/*var filterRes = pokemonRepository.getAll().filter(item => item.height > 9);
+console.log(filterRes);*/
 
 
 
